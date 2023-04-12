207426f72530d5c22ff6232df3ef4aafe89af693
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-12 15:35:45 -0700

    pair programming
    worked with @lianaa98

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,35 +4,47 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
 
+  const matrix = [
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ];
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch(matrix, 'FRANK')
     assert.isFalse(result);
   });
 
   it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
+    const result = wordSearch(matrix, 'SEINFELD')
     assert.isTrue(result);
   });
+
+  it("should return true if the word can be found vertically", () => {
+    const result = wordSearch(matrix, 'YAAA');
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not found vertically", () => {
+    const result = wordSearch(matrix, 'NOTHERE');
+    assert.isFalse(result);
+  });
+
+  it("should return false if the array is empty", () => {
+    const result = wordSearch(matrix, 'TEST');
+    assert.isFalse(result);
+  });
+
+  it("should return false if word is an empty string", () => {
+    const result = wordSearch(matrix, '');
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,52 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (word === "") return false;
+
+  // check horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  //console.log(horizontalJoin)
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // check vertically
+  const vertical = function(matrix) {
+
+    let newMatrix = [];
+
+    for (let row = 0; row < matrix.length; row++) {
+
+      if (matrix.length === 1) {
+        matrix[0].forEach(num => {
+          let arr2 = [];
+          arr2.push(num);
+          newMatrix.push(arr2);
+        });
+        return newMatrix;
+      }
+
+      let arr = [];
+
+      for (let col = 0; col < matrix.length; col++) {
+        if (matrix[col][row] !== undefined) {
+          arr.push(matrix[col][row]);
+        }
+      }
+      newMatrix.push(arr);
+    }
+    return newMatrix;
+  };
+
+  const verticalJoin = vertical(letters).map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // found nothing ...
+  return false;
+
+};
+
+module.exports = wordSearch;
